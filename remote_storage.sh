afbeb50145c2f755f300294b6c74678215ebe97c
rajat430 <rajatbhawsar430@gmail.com>
2026-08-08 14:32:29 +0530

    added dvc

--- remote_storage.sh
+++ remote_storage.sh
@@ -1,8 +1,14 @@
-# dvc init
+#remove existing dvc config and initialize dvc
+rm -rf .dvc
+rm -f .dvcignore
+rm dvc.lock
+#Initialize dvc and track the dataset with dvc
+
+dvc init
 git add .dvc .dvcignore
 git commit -m "init dvc"
 dvc add data/raw/
 git commit -m "track dataset with dvc"
 dvc remote add -d myremote azure://data
 dvc remote modify myremote account_name 'mystorage0101010101011'
-dvc remote modify --local myremote account_key ''
+dvc remote modify --local myremote account_key 'abcd'
